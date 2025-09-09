3d8fc5ce303ae7921bd0968b5f6c576f7e198fbf
Divyansh Agrawal <divyansh1001agrawal@gmail.com>
2025-09-10 00:48:15 +0530

    Project completed

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,48 +1,41 @@
-import express from "express";
-import authRoutes from "./routes/auth.routes.js";
-import messageRoutes from "./routes/message.routes.js";
-import dotenv from "dotenv";
-import connectDB from "./db/index.db.js";
-import cookieParser from "cookie-parser";
-import cors from "cors";
-import { app, server } from "./utils/socket.utils.js";
+import express from "express"
+import authRoutes from "./routes/auth.routes.js"
+import messageRoutes from "./routes/message.routes.js"
+import dotenv from "dotenv"
+import connectDB from "./db/index.db.js"
+import cookieParser from "cookie-parser"
+import cors from "cors"
+import { app,server } from "./utils/socket.utils.js"
 
-import path from "path";
-import { fileURLToPath } from "url";
+import path from "path"
 
-dotenv.config();
+dotenv.config()
 
-const PORT = process.env.PORT || 4000;
+const PORT=process.env.PORT || 4000
+const __dirname=path.resolve()
 
-// Fix __dirname for ES modules
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
+app.use(express.json())
 
-app.use(express.json());
-app.use(cookieParser());
+app.use(cookieParser())
 
-app.use(
-  cors({
-    origin: ["http://localhost:5173", "https://mern-chat-5dg4.onrender.com"],
-    credentials: true,
-  })
-);
-
-app.use("/api/auth", authRoutes);
-app.use("/api/message", messageRoutes);
+app.use(cors({
+    origin: ["http://localhost:5173",
+        "https://mern-chat-5dg4.onrender.com",
+    ],
+    credentials:true
+}))
+app.use("/api/auth",authRoutes)
+app.use("/api/message",messageRoutes)
 
 if (process.env.NODE_ENV === "production") {
-  // Absolute path to frontend build
-  const frontendPath = path.join(__dirname, "../frontend/dist");
-  app.use(express.static(frontendPath));
+  app.use(express.static(path.join(__dirname, "../frontend/dist")));
 
-  // Catch-all route
   app.get("*", (req, res) => {
-    res.sendFile(path.join(frontendPath, "index.html"));
+    res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
   });
 }
 
-server.listen(PORT, "0.0.0.0", () => {
-  console.log(`Server is running on port ${PORT}`);
-  connectDB();
-});
+server.listen(PORT,()=>{
+    console.log(`server is running on ${PORT}`);
+    connectDB()  
+})
